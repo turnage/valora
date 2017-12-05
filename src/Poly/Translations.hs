b446f503628808284cdfe08fb1ba788026545b15
Payton Turnage <paytonturnage@gmail.com>
2017-12-04 23:03:42 -0500

    Implement poly translation.

--- src/Poly/Translations.hs
+++ src/Poly/Translations.hs
@@ -1,5 +1,6 @@
 module Poly.Translations
   ( scale
+  , translate
   ) where
 
 import qualified Data.Vector as V
@@ -9,12 +10,16 @@ import Poly (Poly(..))
 import Poly.Properties (centroid)
 
 scale :: Double -> Poly -> Poly
-scale factor Poly {vertices} = Poly {vertices = vertices'}
+scale factor Poly {vertices} = Poly {vertices = V.zipWith (+) vertices deltas}
   where
-    vertices' = V.zipWith (+) vertices deltas
     deltas = V.map (\point -> Point {x = factor', y = factor'} * (point - c)) vertices
     factor' =
       if factor >= 1
         then factor - 1
         else -1 * (1 - factor)
     c = centroid Poly {vertices}
+
+translate :: Point -> Poly -> Poly
+translate dest Poly {vertices} = Poly {vertices = V.map (+ translation) vertices}
+  where
+    translation = dest - (centroid Poly {vertices})
