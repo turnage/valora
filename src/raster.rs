7aec11f1d85a05040e8e3219ff1aa851986b76c8
Payton Turnage <paytonturnage@gmail.com>
2019-08-24 14:44:07 -0700

    Surface type.

--- src/raster.rs
+++ src/raster.rs
@@ -1,21 +1,21 @@
-mod buffer;
 mod grid_lines;
 mod path;
 mod regions;
+mod surface;
 
 #[cfg(test)]
 mod test {
-    use super::buffer::*;
     use super::grid_lines::*;
     use super::path::*;
     use super::regions::*;
+    use super::surface::*;
     use crate::geo::*;
     use image::Pixel;
     use std::convert::TryFrom;
 
     #[test]
     fn image_output() {
-        let mut buffer = Buffer::new(200, 200);
+        let mut surface = Surface::with_dimensions(200, 200);
         let triangle = Polygon::try_from(vec![
             V2::new(0.0, 0.0),
             V2::new(0.0, 100.0),
@@ -32,9 +32,9 @@ mod test {
             match region {
                 Region::Boundary { x, y } => {
                     println!("Shading boundary region {:?}v{:?}", x, y);
-                    buffer
-                        .get_pixel_mut(x as u32, 199 - (y as u32))
-                        .apply(|_| 1.0);
+                    surface
+                        .pixel(x, 199 - y)
+                        .map(|p| p.iter_mut().for_each(|v| *v = 1.0));
                 }
                 e => {
                     println!("Other boundary: {:?}", e);
@@ -42,8 +42,7 @@ mod test {
             }
         }
 
-        finalize_buffer(buffer)
-            .save("test.bmp")
-            .expect("To save buffer");
+        let buffer: FinalBuffer = surface.into();
+        buffer.save("test.bmp").expect("To save surface");
     }
 }
