4ba76e92cde7c8053be11bd65a52aea6bc5d5a36
Payton Turnage <paytonturnage@gmail.com>
2019-08-24 19:41:23 -0700

    Path hit attribution.

--- src/raster.rs
+++ src/raster.rs
@@ -1,6 +1,7 @@
 mod grid_lines;
 mod path;
 mod regions;
+mod sampling;
 mod surface;
 
 #[cfg(test)]
@@ -16,11 +17,11 @@ mod test {
 
     #[test]
     fn image_output() {
-        let mut surface = Surface::with_dimensions(5, 5);
+        let mut surface = Surface::with_dimensions(5000, 5000);
         let triangle = Polygon::try_from(vec![
             V2::new(0.0, 0.0),
-            V2::new(0.0, 3.0),
-            V2::new(3.0, 0.0),
+            V2::new(0.0, 700.0),
+            V2::new(3000.0, 0.0),
         ])
         .expect("triangle");
 
@@ -28,14 +29,14 @@ mod test {
 
         println!("Region list: {:?}", region_list);
 
-        for region in region_list.regions() {
-            println!("Region: {:?}", region);
-            for shade_command in region {
-                println!("\tCommand: {:?}", shade_command);
-                surface
-                    .pixel(shade_command.x, shade_command.y)
-                    .map(|p| p.iter_mut().for_each(|v| *v = 1.0));
-            }
+        for shade_command in region_list.shade_commands() {
+            println!("\tCommand: {:?}", shade_command);
+            surface.pixel(shade_command.x, shade_command.y).map(|p| {
+                p[0] = 1.0;
+                p[1] = 1.0;
+                p[2] = 1.0;
+                p[3] = shade_command.coverage;
+            });
         }
 
         let buffer: FinalBuffer = surface.into();
--- src/raster/regions.rs
+++ src/raster/regions.rs
@@ -1,54 +1,37 @@
 //! Raster region search and enumeration.
 
-use super::{grid_lines::*, path::*};
+use super::{grid_lines::*, path::*, sampling::*};
 use crate::geo::*;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub enum Region {
+enum Region {
     Boundary {
         x: usize,
         y: usize,
+        // path index
+        path: usize,
     },
     Fill {
         start_x: usize,
         end_x: usize,
         y: usize,
+        // path index
+        path: usize,
     },
-}
-
-impl IntoIterator for Region {
-    type IntoIter = ShadeCommandIter;
-    type Item = ShadeCommand;
-    fn into_iter(self) -> Self::IntoIter {
-        match self {
-            Region::Boundary { x, y } => ShadeCommandIter {
-                x,
-                y,
-                end_x: x + 1,
-                super_sample: true,
-            },
-            Region::Fill { start_x, end_x, y } => ShadeCommandIter {
-                x: start_x,
-                y,
-                end_x,
-                super_sample: false,
-            },
-        }
-    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct ShadeCommand {
     pub x: usize,
     pub y: usize,
-    pub super_sample: bool,
+    pub coverage: f64,
 }
 
 pub struct ShadeCommandIter {
     x: usize,
     y: usize,
     end_x: usize,
-    super_sample: bool,
+    coverage: f64,
 }
 
 impl Iterator for ShadeCommandIter {
@@ -60,7 +43,7 @@ impl Iterator for ShadeCommandIter {
             Some(ShadeCommand {
                 x: self.x - 1,
                 y: self.y,
-                super_sample: self.super_sample,
+                coverage: self.coverage,
             })
         } else {
             None
@@ -68,10 +51,17 @@ impl Iterator for ShadeCommandIter {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
+struct PathHit {
+    x: usize,
+    path: usize,
+}
+
+#[derive(Debug, Default)]
 pub struct RegionList {
+    paths: Vec<Vec<MonotonicSegment>>,
     /// Scan lines assumed to have intersections sorted by left to right.
-    scan_lines: Vec<Vec<usize>>,
+    scan_lines: Vec<Vec<PathHit>>,
 }
 
 impl std::iter::FromIterator<Polygon> for RegionList {
@@ -79,7 +69,7 @@ impl std::iter::FromIterator<Polygon> for RegionList {
     where
         T: IntoIterator<Item = Polygon>,
     {
-        let mut list = RegionList { scan_lines: vec![] };
+        let mut list = RegionList::default();
 
         for polygon in iter {
             list.push(polygon);
@@ -87,7 +77,7 @@ impl std::iter::FromIterator<Polygon> for RegionList {
 
         list.scan_lines
             .iter_mut()
-            .for_each(|scan_line| scan_line.sort_unstable());
+            .for_each(|scan_line| scan_line.sort_unstable_by_key(|p| p.x));
 
         list
     }
@@ -95,45 +85,78 @@ impl std::iter::FromIterator<Polygon> for RegionList {
 
 impl RegionList {
     fn push(&mut self, poly: Polygon) {
-        poly.edges()
+        let path: Vec<MonotonicSegment> = poly
+            .edges()
             .map(MonotonicSegment::try_from)
             .filter_map(Result::ok)
-            .for_each(|segment| {
-                let bounds = segment.bounds();
-                println!("Bounds: {:?}", bounds);
-                self.reserve_up_to(bounds.top.ceil() as usize);
+            .collect();
 
-                let iter = GridLinesIter::Bounds(bounds);
-                for (i, horizontal_line) in iter.horizontal().enumerate() {
-                    if let Some(x) = segment.sample(horizontal_line as f64).or_else(|| {
-                        let x = if i == 0 { bounds.bottom } else { bounds.top };
-                        let s = segment.sample(x);
-                        println!("Sampled: {:?}", s);
-                        s
-                    }) {
-                        println!("Sampling at {:?} -> {:?}", horizontal_line, x);
-                        self.mark_intersection(horizontal_line, self.scan_column(x));
-                    }
+        let path_idx = self.paths.len();
+
+        for segment in &path {
+            let bounds = segment.bounds();
+            println!("Bounds: {:?}", bounds);
+            self.reserve_up_to(bounds.top.ceil() as usize);
+
+            let iter = GridLinesIter::Bounds(bounds);
+            for (i, horizontal_line) in iter.horizontal().enumerate() {
+                if let Some(x) = segment.sample(horizontal_line as f64).or_else(|| {
+                    let x = if i == 0 { bounds.bottom } else { bounds.top };
+                    let s = segment.sample(x);
+                    println!("Sampled: {:?}", s);
+                    s
+                }) {
+                    println!("Sampling at {:?} -> {:?}", horizontal_line, x);
+                    self.mark_intersection(
+                        horizontal_line,
+                        PathHit {
+                            x: self.scan_column(x),
+                            path: path_idx,
+                        },
+                    );
                 }
-            });
+            }
+        }
+
+        self.paths.push(path);
     }
 
-    pub fn regions(mut self) -> impl Iterator<Item = Region> {
-        self.scan_lines
-            .into_iter()
-            .enumerate()
-            .flat_map(|(y, mut scan_line)| RegionIter {
-                scan_line,
+    pub fn shade_commands<'a>(&'a self) -> impl Iterator<Item = ShadeCommand> + 'a {
+        self.regions().flat_map(move |region| match region {
+            Region::Boundary { x, y, path } => ShadeCommandIter {
+                x,
                 y,
-                i: 0,
-                last_on_row: None,
-                queue: None,
-            })
+                end_x: x + 1,
+                coverage: coverage(
+                    V2::new(x as f64, y as f64),
+                    SampleDepth::Super4,
+                    &self.paths[path],
+                ),
+            },
+            Region::Fill {
+                start_x, end_x, y, ..
+            } => ShadeCommandIter {
+                x: start_x,
+                y,
+                end_x,
+                coverage: 1.0,
+            },
+        })
     }
 
-    fn mark_intersection(&mut self, row: usize, column: usize) {
+    fn regions<'a>(&'a self) -> impl Iterator<Item = Region> + 'a {
+        (0..(self.scan_lines.len())).flat_map(move |y| RegionIter {
+            region_list: &self,
+            y,
+            i: 0,
+            last_on_row: None,
+            queue: None,
+        })
+    }
+
+    fn mark_intersection(&mut self, row: usize, hit: PathHit) {
         self.reserve_up_to(row);
-        self.scan_lines[row].push(column);
+        self.scan_lines[row].push(hit);
     }
 
     fn reserve_up_to(&mut self, upper_bound: usize) {
@@ -151,39 +174,40 @@ impl RegionList {
     }
 }
 
-struct RegionIter {
-    /// Scan line assumed to have intersections sorted by left to right.
-    scan_line: Vec<usize>,
+struct RegionIter<'a> {
+    region_list: &'a RegionList,
     y: usize,
     i: usize,
     last_on_row: Option<usize>,
     queue: Option<Region>,
 }
 
-impl Iterator for RegionIter {
+impl<'a> Iterator for RegionIter<'a> {
     type Item = Region;
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(queue) = self.queue.take() {
             return Some(queue);
         }
 
-        let next_boundary = self.scan_line.get(self.i)?;
+        let next_boundary = self.region_list.scan_lines[self.y].get(self.i)?.x;
         if let Some(last_on_row) = self.last_on_row.take() {
-            if *next_boundary > last_on_row + 1 {
+            if next_boundary > last_on_row + 1 {
                 self.queue = Some(Region::Fill {
                     start_x: last_on_row + 1,
-                    end_x: *next_boundary,
+                    end_x: next_boundary,
                     y: self.y,
+                    path: 0, // TODO
                 });
             }
         } else {
-            self.last_on_row = Some(*next_boundary);
+            self.last_on_row = Some(next_boundary);
         }
 
         self.i += 1;
         Some(Region::Boundary {
-            x: *next_boundary,
+            x: next_boundary,
             y: self.y,
+            path: 0, // TODO
         })
     }
 }
@@ -210,17 +234,42 @@ mod test {
         assert_eq!(
             regions.regions().collect::<Vec<Region>>(),
             vec![
-                Region::Boundary { x: 0, y: 0 },
-                Region::Boundary { x: 2, y: 0 },
+                Region::Boundary {
+                    x: 0,
+                    y: 0,
+                    path: 0
+                },
+                Region::Boundary {
+                    x: 2,
+                    y: 0,
+                    path: 0
+                },
                 Region::Fill {
                     start_x: 1,
                     end_x: 2,
-                    y: 0
+                    y: 0,
+                    path: 0
                 },
-                Region::Boundary { x: 0, y: 1 },
-                Region::Boundary { x: 1, y: 1 },
-                Region::Boundary { x: 0, y: 2 },
-                Region::Boundary { x: 0, y: 2 }
+                Region::Boundary {
+                    x: 0,
+                    y: 1,
+                    path: 0
+                },
+                Region::Boundary {
+                    x: 1,
+                    y: 1,
+                    path: 0
+                },
+                Region::Boundary {
+                    x: 0,
+                    y: 2,
+                    path: 0
+                },
+                Region::Boundary {
+                    x: 0,
+                    y: 2,
+                    path: 0
+                }
             ]
         );
     }
