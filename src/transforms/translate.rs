bd53f3f6ce82abe869c5857569ebc0c466408395
Payton Turnage <paytonturnage@gmail.com>
2020-04-14 14:59:17 -0700

    Add `translate_along` to `Translate`

--- src/transforms/translate.rs
+++ src/transforms/translate.rs
@@ -1,11 +1,19 @@
 //! Translations.
 
-use crate::{P2, V2};
+use crate::{Angle, Ellipse, P2, V2};
 
 /// A trait for spatially translatable types.
-pub trait Translate {
+pub trait Translate: Sized {
     /// Translate `self` by the given translation vector.
     fn translate(self, translation: V2) -> Self;
+
+    /// Translate `self` along the given direction.
+    fn translate_along(self, direction: Angle, distance: f32) -> Self {
+        let offset = Ellipse::circle(P2::new(0., 0.), distance)
+            .circumpoint(direction)
+            .to_vector();
+        self.translate(offset)
+    }
 }
 
 impl Translate for P2 {
